c5da4a659197ae8fb9bd0a83418451f7cf55c257
khanghoang1210 <nguyenhoangkhang12102003@gmail.com>
2024-07-20 23:18:36 +0700

    add: forgot password UI

--- web/src/components/auth/AuthModal.tsx
+++ web/src/components/auth/AuthModal.tsx
@@ -3,6 +3,7 @@ import SignIn from "./SignIn";
 import SignUp from "./SignUp";
 import { useRecoilValue, useSetRecoilState } from "recoil";
 import { authModalState } from "@/atoms/authModalAtom";
+import ForgotPassword from "./ForgotPassword";
 
 type AuthModalProps = {};
 
@@ -13,7 +14,7 @@ const AuthModal: React.FC<AuthModalProps> = () => {
     <>
       <div className="flex items-center justify-center pt-5 pb-20">
         <div className="rounded-lg shadow bg-white-900 flex items-center justify-center content-center w-[384px]">
-        {authModal.type ==='login' ? <SignIn/> :authModal.type === 'register'? <SignUp/> : null}
+        {authModal.type ==='login' ? <SignIn/> :authModal.type === 'register'? <SignUp/> : <ForgotPassword/>}
         </div>
       </div>
     </>
--- web/src/components/auth/ForgotPassword.tsx
+++ web/src/components/auth/ForgotPassword.tsx
@@ -2,17 +2,19 @@ import { authModalState } from '@/atoms/authModalAtom';
 import React from 'react';
 import { useSetRecoilState } from 'recoil';
 
-type SignInProps = {
+type ForgotPasswordProps = {
     
 };
 
-const SignIn:React.FC<SignInProps> = () => {
+const ForgotPassword:React.FC<ForgotPasswordProps> = () => {
     const setAuthModalState = useSetRecoilState(authModalState);
     const handleClick = (type:"login"|"register"|"forgotPassword") =>{
         setAuthModalState((prev) =>({...prev, type}));
     }
     
     return <form className=' space-y-6 px-7 pt-10 pb-4  w-[384px] h-[350px]'>
+        <p className='justify-center flex text-black-900 font-medium text-base'>Forgot Password</p>
+        <p className='justify-center items-center text-center flex text-black-500 font-normal text-base'>Enter your email to reset your password and access your account.</p>
 
     <div>
         <input
@@ -26,40 +28,16 @@ const SignIn:React.FC<SignInProps> = () => {
             placeholder='Email'
         />
     </div>
-    <div>
-       
-        <input
-     
-            type='password'
-            name='password'
-            id='password'
-            className='
-    border-2 border-white-400 outline-none sm:text-sm rounded-lg block w-full p-2.5
-    bg-gray-600 text-white
-'
-            placeholder='Password'
-        />
-    </div>
-
+  
     <button
         type='submit'
         className='w-full text-white focus:ring-blue-300 font-medium rounded-lg
         text-sm text-white-900 px-5 py-2.5 text-center bg-black-900 hover:bg-black-800
     '
     >
-        Login
+        Send Reset Link
     </button>
-    <button className='flex w-full justify-end'>
-        <a href='#' className='text-sm block text-black-500 hover:underline w-full text-right' onClick={()=>handleClick("forgotPassword")}>
-            Forgot Password?
-        </a>
-    </button>
-    <div className='text-sm text-black-600'>
-        Don&apos;t have an account? &nbsp;{" "}
-        <a href='#' className='text-black-800 hover:underline font-medium' onClick={()=>handleClick("register")}>
-           Sign Up
-        </a>
-    </div>
+   
 </form>
 }
-export default SignIn;
+export default ForgotPassword;
